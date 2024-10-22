1e10fb9abaef3c6d606fa050a6d2f365d379006b
Thadryan Sweeney <thadryan@protonmail.com>
2024-10-22 11:13:00 -0400

    todo up to date

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,3 +1,11 @@
+# https://julianhofer.eu/blog/01-silverblue-nix/
+# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+# nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+# nix-channel --update
+# nix-shell '<home-manager>' -A install
+
 { config, pkgs, ... }:
 
 {
