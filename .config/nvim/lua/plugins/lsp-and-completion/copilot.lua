6cbb28cc1d8c3602db76fb8507db2439ef4bc472
Thadryan Sweeney <thadryan@protonmail.com>
2024-10-22 10:41:08 -0400

    minor tweak

--- .config/nvim/lua/plugins/lsp-and-completion/copilot.lua
+++ .config/nvim/lua/plugins/lsp-and-completion/copilot.lua
@@ -3,7 +3,7 @@ return {
         "github/copilot.vim",
         enabled = true,
         config = function()
-            vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("")', {expr=true, noremap=true, script=true})
+            vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("")', {expr=true, noremap=true, script=true})
             -- how does this not work :(
             -- require("copilot").setup({
             --     autostart = false
