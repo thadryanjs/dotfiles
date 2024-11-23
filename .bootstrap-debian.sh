952044f2f1af709eb9494681a3382f4136b6c372
thadryanjs <thadryan@protonmail.com>
2024-11-23 10:04:31 -0500

    before move

--- .bootstrap-debian.sh
+++ .bootstrap-debian.sh
@@ -1,9 +1,8 @@
-
 # Phase 1: "Manual"
 # start this running (we're gonna replace it all but switching from an outdated system makes me nervous)
 sudo apt update && sudo apt upgrade --yes
 sudo upgrade dist
-sudo apt install stow git vim-gtk3 systemd-coredump
+sudo apt install stow git vim-gtk3
 # make sure you're good to go with keys while it does
 ssh-keygen -t ed25519 -C "thadryan@protonmail.com"
 cat ~/.ssh/id_ed25519.pub
@@ -47,7 +46,7 @@ flatpak install vivaldi ferdium discord zotero obsidian gimp spotify sleek app/o
 
 # if you're using nix for package managment
 # install your minimal base stuff and stuff that needs sudo
-# sudo apt install kitty vim-gtk3 cifs-utils openconnect flatpak fuse --yes
+sudo apt install kitty vim-gtk3 cifs-utils openconnect flatpak fuse --yes
 # https://julianhofer.eu/blog/01-silverblue-nix/
 # get home-manager running:
 # remove stuff that will be nixified
@@ -62,7 +61,7 @@ flatpak install vivaldi ferdium discord zotero obsidian gimp spotify sleek app/o
 
 # if not using nix
 # install your stuff (stow, vim-gtk3, and git are already here)
-sudo apt install neovim fzf tmux kitty distrobox python3-venv gcc flatpak podman cifs-utils openconnect wget silversearcher-ag ripgrep bat fonts-ubuntu nodejs fuse tree r-base npm default-jre grub-customizer --yes
+# sudo apt install neovim fzf tmux kitty distrobox python3-venv gcc flatpak podman cifs-utils openconnect wget silversearcher-ag ripgrep bat fonts-ubuntu nodejs fuse tree r-base npm default-jre grub-customizer --yes
 # sudo apt uninstall neovim fzf tmux distrobox python3-venv gcc podman wget silversearcher-ag ripgrep bat fonts-ubuntu nodejs tree r-base npm default-jre --yes
 
 # tresorit
@@ -77,17 +76,16 @@ chmod -u+xrw tresorit_installer.run
 # basic env for data science ad LSPs
 micromamba create --name .dcsi-mmamba-env.yaml --yes
 
-# this
+# this?
 # curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
-# gnome tweaks
+# If on gnome, make a few tweaks
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 
 # after tresorit is up-to-date
-sudo cp ~/Documents/WorkVault/Areas/Tech/Areas/GNOME/Jolly-Downloads/Jolly-Dark-Icons /usr/share/icons
-
+# sudo cp ~/Documents/WorkVault/Areas/Tech/Areas/GNOME/Jolly-Downloads/Jolly-Dark-Icons /usr/share/icons
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/DejaVuSansMono.zip
 
