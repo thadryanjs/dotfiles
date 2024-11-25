72fdb0604299ef692467e4130f328ecc13a58a9b
thadryanjs <thadryan@protonmail.com>
2024-11-25 06:43:00 -0500

    minor tweaks

--- .scripts/update-debian-nix.sh
+++ .scripts/update-debian-nix.sh
@@ -1,9 +1,10 @@
 sudo apt update
 sudo apt upgrade
 sudo apt upgrade dist
+sudo apt auto-remove
 nix-channel --update
 home-manager switch
 nix-env --delete-generations +3
 nix-collect-garbage
 nix store optimise
-flatpak update --yes
+flatpak update
