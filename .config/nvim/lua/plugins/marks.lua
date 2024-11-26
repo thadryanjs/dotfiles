7a0df32280d66ded07ae92bd3cb50eab4a139c5d
thadryanjs <thadryan@protonmail.com>
2024-11-26 10:29:18 -0500

    fixed conflict, cleanup

--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -11,12 +11,8 @@ return {
                     delete_buf = "<leader>mr"
                 }
             }
-            -- these didn't work
-            -- vim.api.nvim_set_keymap('n', '<leader>mm', "<cmd>lua require('marks').toggle_mark()<CR>", { noremap = true, silent = true })
-            -- vim.api.nvim_set_keymap('n', '<leader>mn', "<cmd>lua require('marks').next_mark()<CR>", { noremap = true, silent = true })
-            -- vim.api.nvim_set_keymap('n', '<leader>mp', "<cmd>lua require('marks').prev_mark()<CR>", { noremap = true, silent = true })
             vim.api.nvim_set_keymap('n', '<leader>mf', ":Telescope marks <CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>mt', ":MarksToggleSigns <CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>ms', ":MarksToggleSigns <CR>", { noremap = true, silent = true })
             vim.api.nvim_set_keymap('n', '<leader>ma', ":MarksListAll <CR>", { noremap = true, silent = true })
             vim.api.nvim_set_keymap('n', '<leader>mb', ":MarksListBuf <CR>", { noremap = true, silent = true })
         end
