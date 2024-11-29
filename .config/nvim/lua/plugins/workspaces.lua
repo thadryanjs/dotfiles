5740b67402c749bb1360a2f5926b610b3f9329fd
thadryanjs <thadryan@protonmail.com>
2024-11-29 06:54:39 -0500

    workspaces for nvim tracked

--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -3,8 +3,7 @@ return {
         "natecraddock/workspaces.nvim",
         config = function()
             require("workspaces").setup({
-                path = "/home/thadryan/Documents/WorkVault/Resources/Templates/workspaces.txt"
-                -- path = "/home/thadryan/.config/workspaces.txt"
+                path = "/home/thadryan/.workspaces.txt"
             })
 
         vim.api.nvim_set_keymap("n", "<leader>wf", ":Telescope workspaces<CR>", { noremap = true, silent = true })
