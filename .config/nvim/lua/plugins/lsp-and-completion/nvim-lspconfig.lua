45e104358db93d3a5bc1b5ba41613024f6ec3d22
thadryanjs <thadryan@protonmail.com>
2024-11-29 09:43:00 -0500

    tex lsp now incorporates spell dict :)

--- .config/nvim/lua/plugins/lsp-and-completion/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/lsp-and-completion/nvim-lspconfig.lua
@@ -6,21 +6,26 @@ return {
             local lspconfig = require('lspconfig')
             local coq = require('coq')
 
-            -- lspconfig.gopls.setup(coq.lsp_ensure_capabilities({
-            --     autostart = false
-            -- }))
+            local words = {}
+            for word in io.open(vim.fn.stdpath("config") .. "/spell/en.utf-8.add", "r"):lines() do
+            	table.insert(words, word)
+            end
+
             lspconfig.r_language_server.setup(coq.lsp_ensure_capabilities({
                 autostart = false
             }))
             lspconfig.pyright.setup(coq.lsp_ensure_capabilities({
                 autostart = false
             }))
-            -- lspconfig.clangd.setup(coq.lsp_ensure_capabilities({
-            --     autostart = false
-            -- }))
             lspconfig.ltex.setup(coq.lsp_ensure_capabilities({
-                use_spellfile = true,
-                autostart = false
+                autostart = true,
+                settings = {
+                    ltex = {
+                        dictionary = {
+                            ['en-US'] = words,
+                        },
+                    },
+                }
             }))
             lspconfig.julials.setup(coq.lsp_ensure_capabilities({
                 autostart = false
@@ -30,13 +35,7 @@ return {
                 autostart = false
             }))
 
-            lspconfig.rust_analyzer.setup(coq.lsp_ensure_capabilities({
-                autostart = false
-            }))
-            -- lspconfig.nimls.setup(coq.lsp_ensure_capabilities({
-            --     autostart = false
-            -- }))
-            -- lspconfig.kotlin_language_server.setup(coq.lsp_ensure_capabilities({
+            -- lspconfig.clangd.setup(coq.lsp_ensure_capabilities({
             --     autostart = false
             -- }))
         end
