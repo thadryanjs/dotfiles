c87333aec8d73df0b794980aa2f4a0373cf5ba95
thadryanjs <thadryan@protonmail.com>
2025-08-31 15:34:32 -0400

    everything awesome

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -24,6 +24,7 @@
     fzf
     atuin
     yazi
+    pixi
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
--- .config/silverblue/rebuild.sh
+++ .config/silverblue/rebuild.sh
@@ -1,3 +1,4 @@
 VERSION_TAG=$(date +%Y-%m-%d-%H-%M)
-sudo podman build --no-cache -t custom-silverblue-gnome:$VERSION_TAG .
+sudo podman build -t custom-silverblue-gnome:$VERSION_TAG .
+# sudo podman build --no-cache -t custom-silverblue-gnome:$VERSION_TAG .
 sudo bootc switch --transport containers-storage localhost/custom-silverblue-gnome:$VERSION_TAG
