356eb8fb1980fd2c163486d42201939b8baa2e89
thadryanjs <thadryan@protonmail.com>
2025-08-31 15:08:50 -0400

    nice

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -17,10 +17,12 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkg; [
+    tmux
+    neovim
+    gvim
+    fzf
+    atuin
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
